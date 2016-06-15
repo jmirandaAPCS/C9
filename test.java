6a3221cdc0247d959d041c1298b1d1727b112b77
jmiranda <josh.a.miranda@gmail.com>
2016-06-15 13:02:28 +0000

    First commit

---
+++ test.java
@@ -0,0 +1,8 @@
+public class test{
+    public static void main(String [] args){
+        System.out.println("hello");
+        test2 ob = new test2(8);
+        System.out.println(ob.getX());
+        
+    }
+}
---
+++ test2.java
@@ -0,0 +1,9 @@
+public class test2{
+    private int x;
+    public test2(int num){
+        x = num;
+    }
+    public int getX(){
+        return x;
+    }
+}
